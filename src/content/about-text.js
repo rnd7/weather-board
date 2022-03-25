6e79a829d8faa073cc7cc1d95d874a52408c0e46
rnd7 <rnd7@riseup.net>
2022-03-25 18:32:08 +0100

    Renamed website directory to content

--- src/content/about-text.js
+++ src/content/about-text.js
@@ -1,6 +1,6 @@
 export default `<b>WTF?</b>
 
-The Weather Board is on of my leisure projects. I liked the idea of an unmoderated shoutbox as in the early days of the internet. I was bored. Here it is.
+The Weather Board is on of my leisure projects. I liked the idea of an unmoderated shoutbox as in the early days of the internet. I was bored. Here it is. Somehow different.
 
 My motivations were those of a programmer, if you deal with web technology you may be interested. Visit my github for more information <a href="https://github.com/rnd7/weather-board">https://github.com/rnd7/weather-board</a>.
 
